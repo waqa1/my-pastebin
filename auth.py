ea1a0a1b6fae2b71b1e89cee08138488b459f7c3
waqa1 <investtmnsu@gmail.com>
2026-01-07 22:38:44 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,7 @@
-ADMIN_PASSWORD = "!Qw459348788"  # ПАРОЛЬ ДЛЯ ДОСТУПА В АДМИНКУ
+import os
+
+# Пароль будет задан в настройках Render
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "fallback_password_123")
 
 def check_password(password):
     return password == ADMIN_PASSWORD
